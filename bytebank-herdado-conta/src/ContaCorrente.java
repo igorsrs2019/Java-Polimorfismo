bc5585895b877315794a479bbf34a84c859351f6
igorsrs2019 <ighor_souza@hotmail.com>
2021-11-08 17:13:55 -0300

    Poliformismo

---
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -0,0 +1,16 @@
+
+//New ContaCorrente
+public class ContaCorrente extends Conta {
+
+	
+	public ContaCorrente(int agencia, int numero) {
+		super(agencia, numero);
+	}
+	
+	@Override
+		public boolean saca(double valor) {
+			double ValorASacar = valor + 0.2;
+			return super.saca(ValorASacar);
+			
+		}
+}
